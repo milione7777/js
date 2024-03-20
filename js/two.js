172d9be54b19ca8a5740d959cd895b6c1b21e3fc
Artem <vsezdes021@gmail.com>
2024-03-20 20:53:43 +0200

    first commit

---
+++ js/two.js
@@ -0,0 +1,29 @@
+const firstname = "Stanko";
+const tab = ' '
+const name = "Artem";
+let number = 15;
+
+number++;
+number++;
+number++;
+number++;
+number++;
+number++;
+number++;
+number++;
+number++;
+
+console.log(firstname);
+console.log(name);
+console.log(number);
+console.log(firstname + tab + name);
+
+const niger = 'хучеш в чоло';
+const result = confirm(niger);
+result ? alert('ну всьо пізда тобі') : alert('нота йди нахуй');
+
+console.log (result);
+
+ 
+
+
